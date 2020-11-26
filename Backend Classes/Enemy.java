8e98d428dd44e51a015b272b3de7d4f01aa4fc88
bennit6 <74574676+bennit6@users.noreply.github.com>
2020-11-26 15:56:58 -0500

    Update Enemy.java
    Adde implementation

--- Backend Classes/Enemy.java
+++ Backend Classes/Enemy.java
@@ -2,11 +2,26 @@
  * Creates enemy object, contains position
  * Need to implement attacks/hit detection
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Benni Trachtenberg
+ * @version 11/26/2020
  */
 
 public class Enemy extends Char
 {
+    private int health;
+    
+    public Enemy()                          // Enemy constructor
+    {
+        health=1;
+    }
 
+    public void setHealth(int amt)          // change the health of the enemy
+    {
+        health = amt;
+    }
+
+    public int getHealth()                  // returns the health of the enemy
+    {
+        return health;
+    }
 }
