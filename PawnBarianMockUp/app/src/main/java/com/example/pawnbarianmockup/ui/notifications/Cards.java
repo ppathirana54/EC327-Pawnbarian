8cdfc29292895853d846b7832e3019b2910f3d7d
Andrew Chen <achen17@bu.edu>
2020-12-02 16:51:30 -0500

    UI, Enemy (Skeleton), Health Bar, End Turn
    Added Jaden's updated Cards.java as well as the Char.java into the ui.notifications file. Assume all of our backend java files will go there in the future. Added in health bar, three images of hearts, their ids are health0, health1, and health2 respectively going from left to right. Added in full heart, half heart and empty heart drawables, this will allow for updating the images to match player health upon hit. Also added in the end turn button to allow for signal flag for enemy to move.

---
+++ PawnBarianMockUp/app/src/main/java/com/example/pawnbarianmockup/ui/notifications/Cards.java
@@ -0,0 +1,125 @@
+package com.example.pawnbarianmockup.ui.notifications;
+/**
+ * Creates an object of the Piece Cards, contains the type.
+ *
+ * Need to implement a method to return potential moves
+ *
+ */
+
+
+import java.util.Hashtable;
+import static java.lang.Math.abs;
+//import java.util.ArrayList;
+
+
+public class Cards
+{
+    private Hashtable<Integer, String> piecesList = new Hashtable<Integer, String>();
+    private int piece;
+    // private ArrayList<String> mods?
+
+    public Cards()
+    {
+        piecesList.put(1, "Pawn");
+        piecesList.put(2, "Knight");
+        piecesList.put(3, "Bishop");
+        piecesList.put(4, "Rook");
+        piecesList.put(5, "Queen");
+        piecesList.put(6, "King");
+
+        this.piece = piece;
+    }
+
+    public String getPiece()
+    {
+        return piecesList.get(piece);
+    }
+
+    public void setPeice(int piece)
+    {
+        this.piece = piece;
+    }
+
+    public boolean getMovement(int[] initPOS, int[] finalPOS, int piece)
+    {
+        //Pawn
+        if(piece == 1)
+        {
+            //needs position of enemy
+        }
+
+        //Knight
+        else if(piece == 2)
+        {
+            int x = finalPOS[0] - initPOS[0];
+            int y = finalPOS[1] - initPOS[1];
+
+            if ((abs(x) == 3 && abs(y) == 1) || (abs(y) == 3 && abs(x) == 1)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        //Bishop
+        else if(piece == 3)
+        {
+            int x = finalPOS[0] - initPOS[0];
+            int y = finalPOS[1] - initPOS[1];
+
+            if (abs(x) == abs(y)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        //Rook
+        else if(piece == 4)
+        {
+            if (((initPOS[0] == finalPOS[0]) && (initPOS[1] != finalPOS[1])) || ((initPOS[1] == finalPOS[1]) && (initPOS[0] != finalPOS[0]))){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        //Queen
+        else if(piece == 5)
+        {
+            int x = finalPOS[0] - initPOS[0];
+            int y = finalPOS[1] - initPOS[1];
+
+            if (initPOS[0] == finalPOS[0] || initPOS[1] == finalPOS[1]){
+                return true;
+            }
+            else if (abs(x) == abs(y)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        //King
+        else if(piece == 6)
+        {
+            int x = finalPOS[0] - initPOS[0];
+            int y = finalPOS[1] - initPOS[1];
+
+            if (abs(x + y) == 1){
+                return true;
+            }
+            else if ((abs(x) == 1 && abs(y) == 1)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return false;
+    }
+}
---
+++ PawnBarianMockUp/app/src/main/java/com/example/pawnbarianmockup/ui/notifications/Char.java
@@ -0,0 +1,68 @@
+package com.example.pawnbarianmockup.ui.notifications;
+
+/**
+ * Holds Position and Can Move
+ * Complete!
+ *
+ * @Priyan Pathirana
+ * @1.0 b
+ */
+public class Char
+{
+    // instance variables - replace the example below with your own
+    private int pos[];
+    private String name;
+    final int INITIAL_STARTING_POS = 2;
+
+    /**
+     * Constructor for objects of class Char
+     */
+    public Char()
+    {
+        pos = new int[]{INITIAL_STARTING_POS,INITIAL_STARTING_POS};
+        this.name = "Dummy";
+    }
+
+    public Char(String init_name)
+    {
+        pos = new int[]{INITIAL_STARTING_POS,INITIAL_STARTING_POS};
+        this.name = init_name;
+
+    }
+
+
+    public Char(int xpos, int ypos, String init_name)
+    {
+        pos = new int[]{xpos,ypos};
+        this.name = init_name;
+    }
+
+    public int[] getPos()
+    {
+        return pos;
+    }
+
+    public void moveTo(int x, int y)
+    {
+        pos[0] = x;
+        pos[1] = y;
+    }
+
+    public void move(int x, int y)
+    {
+        pos[0] = pos[0] + x;
+        pos[1] = pos[1] + y;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String newName)
+    {
+        name = newName;
+    }
+
+
+}
